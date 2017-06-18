379e2be02009e1d482ef86209e1d26371e7481b8
Ashok Dey <ashokdey100@gmail.com>
2017-06-19 03:22:10 +0530

    separate UserTable component

--- app/ts/app.tsx
+++ app/ts/app.tsx
@@ -1,40 +1,7 @@
 import 'core-js';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-class UserTable extends React.Component<{}, {}> {
-    public render () {
-        return (
-            <div>
-                <h1>All the users</h1>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>CreatedOn</th>
-                            <th>Usage</th>
-                            <th>Plan</th>
-                            <th>Status</th>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td>123sadas223po43a</td>
-                            <td>Ashok Dey</td>
-                            <td>ashokdey@gmail.com</td>
-                            <td>21-12-12</td>                            
-                            <td>0.2</td>
-                            <td>Premium</td>
-                            <td>Active</td>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
-        );
-    }
-}
+import { UserTable } from './components/UserTable'
 
 import '../scss/style.scss';
 
---
+++ app/ts/components/UserTable.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+export class UserTable extends React.Component<{}, {}> {
+    public render () {
+        return (
+            <div>
+                <h1>All the users</h1>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>CreatedOn</th>
+                            <th>Usage</th>
+                            <th>Plan</th>
+                            <th>Status</th>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td>123sadas223po43a</td>
+                            <td>Ashok Dey</td>
+                            <td>ashokdey@gmail.com</td>
+                            <td>21-12-12</td>                            
+                            <td>0.2</td>
+                            <td>Premium</td>
+                            <td>Active</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        );
+    }
+}
