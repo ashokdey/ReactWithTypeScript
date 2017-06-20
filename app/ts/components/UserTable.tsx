c9316aacbba6102ff5ff94a3519291e893514f60
Ashok Dey <ashokdey100@gmail.com>
2017-06-21 01:14:15 +0530

    added sorting feature by clicking on Name, Email, Usage, etc

--- app/ts/components/UserTable.tsx
+++ app/ts/components/UserTable.tsx
@@ -1,26 +1,40 @@
 import * as React from 'react';
 import {User} from '../models/user';
 
+enum UserTableCols {
+    None, Name, Email, Usage, Plan, Status
+}
+
+interface UserTableState {
+    sortCol : UserTableCols;
+}
+
 interface UserTableProps {
     users : User[];
 }
 
-export class UserTable extends React.Component<UserTableProps, {}> {
+export class UserTable extends React.Component<UserTableProps, UserTableState> {
 
     public static propTypes = {
         users : React.PropTypes.arrayOf(
             React.PropTypes.instanceOf(User),
-        ),
+        )
     };
 
     public static defaultProps = {
-        users : [] as User[],
+        users : [] as User[]
     };
 
     private containerStyle : any;
 
     constructor(props : UserTableProps) {
+        // sending props to the parent 
         super(props);
+
+        // setting the state 
+        this.state = {
+            sortCol : UserTableCols.Name
+        }
 
         this.containerStyle = {
             border : '1px solid lightgray',
@@ -39,16 +53,21 @@ export class UserTable extends React.Component<UserTableProps, {}> {
                 <table className='table table-striped'>
                     <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Email</th>
+                            <th onClick={() => this.onSort(UserTableCols.Name)}
+                                style={this.activeSort(UserTableCols.Name)}>Name</th>
+                            <th onClick={() => this.onSort(UserTableCols.Email)}
+                                style={this.activeSort(UserTableCols.Email)}>Email</th>
                             <th>CreatedOn</th>
-                            <th>Usage</th>
-                            <th>Plan</th>
-                            <th>Status</th>
+                            <th onClick={() => this.onSort(UserTableCols.Usage)}
+                                style={this.activeSort(UserTableCols.Usage)}>Usage</th>
+                            <th onClick={() => this.onSort(UserTableCols.Plan)}
+                                style={this.activeSort(UserTableCols.Plan)}>Plan</th>
+                            <th onClick={() => this.onSort(UserTableCols.Status)}
+                                style={this.activeSort(UserTableCols.Status)}>Status</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.users.map(
+                        {this.props.users.concat().sort(this.getSortFn()).map(
                             (user : User) => {
                                 return (
                                     <tr key={user.id}>
@@ -67,4 +86,27 @@ export class UserTable extends React.Component<UserTableProps, {}> {
             </div>
         );
     }
+
+    private onSort(sortCol : UserTableCols) : void {
+        this.setState({ sortCol : sortCol});
+    }
+
+    private activeSort(sortCol : UserTableCols) : any {
+        return {
+            color : this.state.sortCol === sortCol ? 'seagreen' : 'black'
+        };
+    }
+
+    private getSortFn() : (a: any, b: any) => number {
+        if(this.state.sortCol === UserTableCols.None) {
+            return (a: any, b:any ) => 0;
+        }
+
+        const fieldName: string = UserTableCols[this.state.sortCol].toLowerCase();
+        return (a: any, b:any ) : number => {
+            if(a[fieldName] < b[fieldName]) return -1;
+            if(a[fieldName] > b[fieldName]) return 1;
+            return 0;
+        } 
+    } 
 }
